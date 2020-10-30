a265230c08694d6cf303d7b6b83b60e62b4f411a
BenStrawbridge <lute_midnight@yahoo.com>
2020-10-30 09:12:11 -0500

    Method Overloading Exercise

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public static string Add(int x, int y, bool myBool)
+        {
+            if (myBool == true)
+            {
+                if (x + y == 1)
+                {
+                    return "1 dollar";
+                }else
+                {
+                    return $"{x + y} dollars";
+                }
+            } else
+            {
+                return null;
+            }
+        }
+
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(4, 5));
+            Console.WriteLine(Add(4, 5, true));
+        }
+    }
+}
